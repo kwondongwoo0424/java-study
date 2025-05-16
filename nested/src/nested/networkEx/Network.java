5acf73011bd5848dee0b4cde43c1d5a2ab84908a
권동우 <hyeonggyugwon3@gmail.com>
2025-05-16 10:22:37 +0900

    Add files via upload

---
+++ nested/src/nested/networkEx/Network.java
@@ -0,0 +1,17 @@
+package nested.networkEx;
+
+public class Network {
+    public void sendMessage(String text) {
+        NetworkMessage networkMessage = new NetworkMessage(text);
+        networkMessage.print();
+    }
+    public static class NetworkMessage {
+        private String content;
+        public NetworkMessage(String content) {
+            this.content = content;
+        }
+        public void print() {
+            System.out.println(content);
+        }
+    }
+}
